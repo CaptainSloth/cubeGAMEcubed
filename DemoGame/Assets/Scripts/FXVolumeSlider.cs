c82ee11411c660635e85f51710e9d327a32ea0a7
robawesome <robduboff@gmail.com>
2017-09-05 18:36:36 -0700

    Cleaning and Menus
    Cleaned out unused stuff
    
    Added menus for audio settings
    Added Slider
    Broke Slider
    Fixing Slider...

---
+++ DemoGame/Assets/Scripts/FXVolumeSlider.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FXVolumeSlider : MonoBehaviour {
+
+    private bool mute;
+    // private AudioSource source;
+    public Slider FXVolume;
+    public AudioSource FXAudio;
+    public void FXVolumeControl(float FXvolumeControl)
+    {
+        AudioListener.volume = FXvolumeControl;
+
+    }
+
+    private void Start()
+    {
+        FXAudio = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            FXAudio.volume = PlayerPrefs.GetInt("Volume");
+        }
+    }
+
+    public void Mute()
+    {
+        mute = !mute;
+        if (mute)
+        {
+            FXAudio.volume = 0;
+        }
+        else
+        {
+            FXAudio.volume = 1;
+        }
+
+        PlayerPrefs.SetFloat("Volume", FXAudio.volume);
+    }
+    
+    void Update () {
+
+        FXAudio.volume = FXVolume.value;
+
+	}
+}
---
+++ DemoGame/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform pauseCanvas;
+    public Transform pauseMenu;
+    public Transform soundSettings;
+    // public Transform controlSettings;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+	}
+
+    public void Pause()
+    {
+        if (pauseCanvas.gameObject.activeInHierarchy == false)
+        {
+            if (pauseMenu.gameObject.activeInHierarchy == false)
+            {
+                pauseMenu.gameObject.SetActive(true);
+                soundSettings.gameObject.SetActive(false);
+            }
+            pauseCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseCanvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void SoundControl(bool soundControlChecked)
+    {
+        if (soundControlChecked)
+        {
+            soundSettings.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+
+           }
+        if (!soundControlChecked)
+        {
+            soundSettings.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+    
+    //        AudioListener.pause = true;
+
+}

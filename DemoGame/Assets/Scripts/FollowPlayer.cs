1a138eabde94c141cfe2ee50945666f96efaa59f
robawesome <robduboff@gmail.com>
2017-09-28 22:24:38 -0700

    Added new models, roads, fixed some things...

--- DemoGame/Assets/Scripts/FollowPlayer.cs
+++ DemoGame/Assets/Scripts/FollowPlayer.cs
@@ -4,9 +4,11 @@ public class FollowPlayer : MonoBehaviour {
 
     public Transform player;
     public Vector3 offset;
+    public float xRot;
 
 	// Update is called once per frame
 	void Update () {
         transform.position = player.position + offset;
-	}
+        Camera.main.transform.rotation = Quaternion.Euler(xRot , 0, 0);
+    }
 }

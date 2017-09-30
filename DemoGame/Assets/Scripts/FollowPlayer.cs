0b2f59b1f8083a1d6cc16fd0ff9c503438bd01c8
robawesome <robduboff@gmail.com>
2017-09-30 12:12:23 -0700

    Fixed car, and camera controls

--- DemoGame/Assets/Scripts/FollowPlayer.cs
+++ DemoGame/Assets/Scripts/FollowPlayer.cs
@@ -6,9 +6,18 @@ public class FollowPlayer : MonoBehaviour {
     public Vector3 offset;
     public float xRot;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
         transform.position = player.position + offset;
-        Camera.main.transform.rotation = Quaternion.Euler(xRot , 0, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(xRot, 0, 0);
     }
+
+
+
+
+
 }
+
+
